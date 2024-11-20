c6fbbe1f6b3e710366bcc0ac5f25b6a41a75b2ab
rodrigog84 <rodrigog.84@gmail.com>
2024-11-20 10:16:40 -0300

    agrega oc externa en codigo 802 de xml

--- core/application/controllers/facturas.php
+++ core/application/controllers/facturas.php
@@ -3438,6 +3438,10 @@ class Facturas extends CI_Controller {
 
                  // var_dump($tipodocumento);
                  // var_dump($id_pedido);
+                  //801:  si es GUIA de pedido
+                                    si tiene OC int, va OC int, sino OC externa
+                          en caso contrario
+                                    va la orden de compra
                   if($tipodocumento == 105 && $id_pedido != 0){
 
                           $this->db->select('ordencompraint')
@@ -3483,6 +3487,11 @@ class Facturas extends CI_Controller {
 
 
                  // exit;
+
+                  //802:
+                        Si tiene OC externa, va la OC externa
+                        En caso contrario, va el pedido
+
 
                   if($opedidoextId != ''){
 
